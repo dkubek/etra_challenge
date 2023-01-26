cff369e1ff03f7edb4da0bc35ccfc6bc65e44704
David Kubek <kubekd@u-pl23.ms.mff.cuni.cz>
2023-01-26 21:02:01 +0100

    Finish hypothesis 1

--- etra.py
+++ etra.py
@@ -25,29 +25,30 @@ class ETRA:
     # URL to ETRA Dataset
     _URL = "http://smc.neuralcorrelate.com/ETRA2019/ETRA2019Challenge.zip"
 
-    def __init__(self, data_dir: str = "data", dataset: str = "etra") -> None:
+    def __init__(self, data_dir: str = "data", dataset: str = "etra", download=True) -> None:
         self.data_dir = Path.cwd() / data_dir
         self.data_dir.mkdir(parents=True, exist_ok=True)
 
-        data_path = self.data_dir / "{}.zip".format(dataset)
-        if not data_path.exists():
-            print(
-                f"Downloading dataset {dataset} (this can take a while) ... ",
-                file=sys.stderr,
-                end="",
-            )
-            urllib.request.urlretrieve(self._URL, filename=data_path)
-            print("DONE", file=sys.stderr)
-        else:
-            print(f"Dataset {dataset} already downloaded.", file=sys.stderr)
+        if download:
+            data_path = self.data_dir / "{}.zip".format(dataset)
+            if not data_path.exists():
+                print(
+                    f"Downloading dataset {dataset} (this can take a while) ... ",
+                    file=sys.stderr,
+                    end="",
+                )
+                urllib.request.urlretrieve(self._URL, filename=data_path)
+                print("DONE", file=sys.stderr)
+            else:
+                print(f"Dataset {dataset} already downloaded.", file=sys.stderr)
 
-        with ZipFile(data_path, "r") as archive:
-            print(f"Unpacking {dataset}...", file=sys.stderr)
-            for filename in archive.namelist():
-                file_path = self.data_dir / filename
-                if not file_path.exists():
-                    print(f"Extracting\t{filename}", file=sys.stderr)
-                    archive.extract(filename, path=self.data_dir)
+            with ZipFile(data_path, "r") as archive:
+                print(f"Unpacking {dataset}...", file=sys.stderr)
+                for filename in archive.namelist():
+                    file_path = self.data_dir / filename
+                    if not file_path.exists():
+                        print(f"Extracting\t{filename}", file=sys.stderr)
+                        archive.extract(filename, path=self.data_dir)
 
 
 def read_data(data_path: Union[str, Path]) -> pd.DataFrame:
